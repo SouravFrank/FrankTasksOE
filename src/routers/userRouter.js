45f440db072691efc8622524a0afdfd70970397d
Sourav2282 <ssadhukhan@gmail.com>
2020-03-30 01:10:11 +0530

    Avatar upload added

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
@@ -82,6 +84,55 @@ router.delete('/users/me', auth, async (req,res) => {
     try {
         await req.user.remove()
         res.send(req.user)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+const upload = multer({
+    // dest: 'avatars/',
+    limits: {
+        fileSize: 8000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            cb(new Error('Please upload image'))
+        }
+        
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) => {
+
+    const buffer = await sharp(req.file.buffer).png().resize({ height: 250, width: 250 }).toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send('Photo uploaded')
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req,res) => {
+    try {
+        req.user.avatar = undefined
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+//Browser compatible API for image view
+router.get('/users/:id/avatar', async (req,res) => {
+    const user = await User.findById(req.params.id)
+    try {
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type','image/png')
+        res.send(user.avatar)
     } catch (error) {
         res.status(500).send(error)
     }
